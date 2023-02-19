99de925b61260c66ce65375fd13e2e2b2032b329
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-02-19 23:48:09 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-log_dir="/LOG"
-backup_dir="/BACKUP"
+log_dir="/path/to/log"
+backup_dir="/path/to/backup"
 occupancy_threshold=70
 
 occupancy=$(df -h "$log_dir" | awk '{print $5}' | tail -1 | sed 's/%//')
 
-if [[ $occupancy -gt $occupancy_threshold ]]; then
+if [ $occupancy -gt $occupancy_threshold ]; then
   # Archive files in the backup directory
   tar -czf "$backup_dir/log_archive_$(date +%Y%m%d%H%M%S).tar.gz" "$log_dir"/*
   # Delete files in the log directory
