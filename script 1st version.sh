9fb4f2b4eef3cc585bea97be3e2e927082b9145a
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-03-19 16:00:43 +0300

    upd

--- script 1st version.sh
+++ script 1st version.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
-log_dir="/LOG"
-backup_dir="/BACKUP"
-occupancy_threshold=70
-occupancy=$(df -h "$log_dir" | awk '{print $5}' | tail -1 | sed 's/%//')
+log_dir = "/LOG"
+backup_dir = "/BACKUP"
+occupancy_threshold = 70
+n = 10
+occupancy = $(df -h "$log_dir" | awk '{print $5}' | tail -1 | sed 's/%//')
 if [[ $occupancy -gt $occupancy_threshold ]]; then
-  # Archive files in the backup directory
-  tar -czf "$backup_dir/log_archive_$(date +%Y%m%d%H%M%S).tar.gz" "$log_dir"/*
-  # Delete files in the log directory
-  rm -rf "$log_dir"/*
+  # Sorting
+  files = ($(ls -1t "$log_dir"))
+  # Selecting
+  files = ("${files_to_archive[@]:0:$n}")
+  # Archiving
+  tar -czf "$backup_dir/log_archive_$(date +%Y%m%d%H%M%S).tar.gz" "${files_to_archive[@]/#/$log_dir/}"
+  # Deleting
+  rm -f "${files_to_archive[@]/#/$log_dir/}"
 fi
