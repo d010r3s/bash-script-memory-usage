54266bdbc7f5b79155fd3eb42d04da3376bec59d
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-03-19 18:29:06 +0300

    corrected

--- script 1st version.sh
+++ script 1st version.sh
@@ -7,9 +7,9 @@ n = 10
 occupancy = $(df -h "$log_dir" | awk '{print $5}' | tail -1 | sed 's/%//')
 if [[ $occupancy -gt $occupancy_threshold ]]; then
   # Sorting
-  files = ($(ls -1t "$log_dir"))
+  files_to_archive = ($(ls -1t "$log_dir"))
   # Selecting
-  files = ("${files_to_archive[@]:0:$n}")
+  files_to_archive = ("${files_to_archive[@]:0:$n}")
   # Archiving
   tar -czf "$backup_dir/log_archive_$(date +%Y%m%d%H%M%S).tar.gz" "${files_to_archive[@]/#/$log_dir/}"
   # Deleting
