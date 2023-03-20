ab8085ef581eb938a9dc4fca2dc6b0a04f8f7523
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-03-20 13:22:25 +0300

    upd

--- script 1st version.sh
+++ script 1st version.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-log_dir="/LOG"
-backup_dir="/BACKUP"
+log_dir="/home/vboxuser/log2"
+backup_dir="/home/vboxuser/backup"
 occupancy_threshold=70
-n=10
+n=1
 occupancy=$(df -h "$log_dir" | awk '{print $5}' | tail -1 | sed 's/%//')
-if [[ $occupancy -gt $occupancy_threshold ]]; then
-  # Sorting
-  files_to_archive=($(ls -1t "$log_dir"))
-  # Selecting
-  files_to_archive=("${files_to_archive[@]:0:$n}")
-  # Archiving
-  tar -czf "$backup_dir/log_archive_$(date +%Y%m%d%H%M%S).tar.gz" "${files_to_archive[@]/#/$log_dir/}"
-  # Deleting
-  rm -f "${files_to_archive[@]/#/$log_dir/}"
+if [[ $occupancy < $occupancy_threshold ]]; then
+	files_to_archive=($(ls -1t "$log_dir"))
+	files_to_archive=("${files_to_archive[@]:0:$n}")
+	#mkdir -p "$backup_dir/log_archive_$(date +%Y%m%d%H%M)"
+	tar -czf $backup_dir/log_archive_$(date +%Y%m%d%H%M).tar.gz ${files_to_archive[@]/#/log_dir/}
+	rm -f "${files_to_archive[@]/#/$log_dir/}"
+	echo "done!"
 fi
