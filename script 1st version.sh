022ca74def4688b5bf49ef487541aadf52a0c989
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-03-20 14:31:13 +0300

    Update script 1st version.sh

--- script 1st version.sh
+++ script 1st version.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-log_dir="/home/vboxuser/Documents/log"
-backup_dir="/home/vboxuser/Documents/backup"
+log_dir="/home/vboxuser/Documents/log/"
+backup_dir="/home/vboxuser/Documents/backup/"
 occupancy_threshold=70
-n=1
+n=2
 occupancy=$(df -h "$log_dir" | awk '{print $5}' | tail -1 | sed 's/%//')
 if [[ $occupancy < $occupancy_threshold ]]; then
 	files_to_archive=($(ls -1t "$log_dir"))
 	files_to_archive=("${files_to_archive[@]:0:$n}")
 	#mkdir -p "$backup_dir/log_archive_$(date +%Y%m%d%H%M)"
-	tar -czf $backup_dir/log_archive_$(date +%Y%m%d%H%M).tar.gz ${files_to_archive[@]/#/log_dir/}
+	tar vczf $backup_dir/log_archive_$(date +%Y%m%d%H%M).tar.gz -C "$log_dir" "${files_to_archive[@]}"
 	rm -f "${files_to_archive[@]/#/$log_dir/}"
 	echo "done!"
 fi
